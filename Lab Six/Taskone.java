653bb893f8ef61d196c8f3ce53b01c9c0bbeff9d
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 11:23:14 +0000

    Update Taskone.java

--- Lab Six/Taskone.java
+++ Lab Six/Taskone.java
@@ -1,1 +1,33 @@
 
+class Grandfather {
+    void company() {
+        System.out.println("Grandfather owns a company.");
+    }
+}
+
+class Father extends Grandfather {
+    void car() {
+        System.out.println("Father owns a car.");
+    }
+}
+
+class Son extends Father {
+    void hobby() {
+        System.out.println("Son loves playing football.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating Son object (can access everything)
+        Son s = new Son();
+
+        System.out.println("== Accessing Grandfather, Father and Son Properties ==");
+        s.company();  // From Grandfather
+        s.car();      // From Father
+        s.hobby();    // From Son
+    }
+}
+
+
