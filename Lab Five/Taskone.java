4a7c1126184bd50224255d89504d878d88f71042
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 11:20:46 +0000

    Update Taskone.java

--- Lab Five/Taskone.java
+++ Lab Five/Taskone.java
@@ -1,1 +1,50 @@
+class BankAccount {
+    String accountNumber;
+    String accountHolderName;
+    double balance;
+
+    // Static variable
+    static String bankName = "Sonali Bank";
+
+    // Constructor to initialize account details
+    BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    // Method to display account information
+    void showAccountInfo() {
+        System.out.println("Bank Name        : " + bankName);
+        System.out.println("Account Number   : " + accountNumber);
+        System.out.println("Account Holder   : " + accountHolderName);
+        System.out.println("Balance          : " + balance);
+        System.out.println("----------------------------");
+    }
+
+    // Static method to display bank name
+    static void showBankName() {
+        System.out.println("Bank Name: " + bankName);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating two bank accounts
+        BankAccount acc1 = new BankAccount("2001", "Rubayet", 59000);
+        BankAccount acc2 = new BankAccount("2003", "Rony", 87000);
+
+        // Display account information
+        System.out.println("== Account 1 Information ==");
+        acc1.showAccountInfo();
+
+        System.out.println("== Account 2 Information ==");
+        acc2.showAccountInfo();
+
+        // Show bank name using static method
+        System.out.println("== Bank Information ==");
+        BankAccount.showBankName();
+    }
+}
 
