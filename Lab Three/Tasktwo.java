1ff3285f546ed653bb2651b538e92534ed6c98d7
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 11:10:47 +0000

    Update Tasktwo.java

--- Lab Three/Tasktwo.java
+++ Lab Three/Tasktwo.java
@@ -1,1 +1,38 @@
-
+import java.util.Scanner; 
+public class PrimeNumbersArray { 
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in); 
+           
+          System.out.print("Enter starting number: ");
+    int start = input.nextInt(); 
+               System.out.print("Enter ending number: ");
+    
+               int end = input.nextInt();
+    
+               int[] primes = new int[(end - start) + 1];
+    int count = 0; 
+               for (int i = start; i <= end; i++) {
+                 
+                if (isPrime(i)) { 
+                  primes[count] = i; 
+                  count++; 
+            } 
+        } 
+        System.out.println("\nPrime numbers in the range:");
+    if (count == 0) { 
+            System.out.println("No prime numbers found."); 
+        } else { 
+               for (int i = 0; i < count; i++) { 
+                 System.out.print(primes[i] + " "); 
+            } 
+        } 
+    } 
+   static boolean isPrime(int n) {
+     if (n <= 1) return false; 
+   for (int i = 2; i <= Math.sqrt(n); i++) {
+     if (n % i == 0) 
+          return false; 
+        } 
+        return true; 
+    } 
+}
