ce472cff4bb47c30c7ec9053fce382e1e5166c2f
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 10:23:23 +0000

    Update TaskOne.java

--- Lab One/TaskOne.java
+++ Lab One/TaskOne.java
@@ -1,1 +1,18 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Rubayet Habib";
+        String studentId = "E251040";
+        int age = 21;
+        String email = "habibrubayet60@gmail.com";
+        String phone = "+8801615374776";
+        String address = "Halishahar, Chattogram";
 
+        System.out.println("Personal Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
