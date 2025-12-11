bc20fa7c6de8024acfeffa20df514187b59516c1
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 11:31:47 +0000

    Update Taskone.java

--- Lab Eight/Taskone.java
+++ Lab Eight/Taskone.java
@@ -1,1 +1,36 @@
+// Parent Class
+class Animal {
+    void sound() {
+        System.out.println("Some generic sound");
+    }
+}
+
+// Dog Class (Child)
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Cat Class (Child)
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+// Main Class
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.sound(); // Some generic sound
+        dog.sound();    // Dog barks
+        cat.sound();    // Cat meows
+    }
+}
 
