f4c203ca3d7deddfe9a7410d3106404459f2bf50
rubayethabib <habibrubayet35@gmail.com>
2025-12-11 11:34:23 +0000

    Update Taskone.java

--- Lab Nine/Taskone.java
+++ Lab Nine/Taskone.java
@@ -1,1 +1,42 @@
+class Student {
+    private String name;
+    private String id;
+    private double cgpa;
+    // Default Constructor
+    public Student() {
+    }
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Rubayet Ahasan Habib");
+         s.setId("E251040");
+        s.setCgpa(4.00)
+        System.out.println("Student Information:");
+        System.out.println("Name: " + s.getName());
+        System.out.println("ID: " + s.getId());
+        System.out.println("CGPA: " + s.getCgpa());
+    }
+}
 
